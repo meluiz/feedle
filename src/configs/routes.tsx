ad8dd1cd68cd4a60c330a0240511c6228cbdcb3a
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2022-06-18 05:59:11 -0300

    feat: route config created

---
+++ src/configs/routes.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'components'
+
+import Homepage from 'pages/home'
+
+const Config = [
+  {
+    path: '/',
+    element: <Layout element={<Homepage />} />,
+  },
+]
+
+export default Config
