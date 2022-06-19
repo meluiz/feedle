671d82d2de0d6cb1e5ad04644dda411218a960d3
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2022-06-19 02:06:52 -0300

    feat: Layout component base created

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,8 +1,8 @@
 import type { LayoutProps } from './type'
 import React, { Fragment, useEffect, useState } from 'react'
 
-import { Loader } from 'components'
-import { Flex } from '@chakra-ui/react'
+import { Header, Loader } from 'components'
+import { Box, Container, Flex, Grid } from '@chakra-ui/react'
 
 const Component: React.FC<LayoutProps> = function ({ element }) {
   const [loading, updateLoading] = useState(true)
@@ -19,7 +19,6 @@ const Component: React.FC<LayoutProps> = function ({ element }) {
         w="100%"
         minH="100vh"
         flexDir="column"
-        alignItems="flex-start"
         justifyContent="flex-start"
         pos="relative"
         bgColor="inherit"
@@ -29,7 +28,26 @@ const Component: React.FC<LayoutProps> = function ({ element }) {
         fontStyle="normal"
       >
         <Header />
-        {element}
+        <Grid w="100%" flex={1} pos="relative" bg="inherit">
+          <Container px={{ base: 0, md: '16px' }}>
+            <Grid w="100%" minH="100%" templateColumns={{ md: '1fr 2fr 1fr' }}>
+              <Box />
+              <Box
+                h="calc(100% + 1px)"
+                bg="mauve.1200"
+                borderLeft={{ lg: '1px solid' }}
+                borderRight={{ lg: '1px solid' }}
+                borderColor={{ lg: 'mauve.1000' }}
+                pos="relative"
+                px="18px"
+                as="main"
+              >
+                {element}
+              </Box>
+              <Box />
+            </Grid>
+          </Container>
+        </Grid>
       </Flex>
     </Fragment>
   )
