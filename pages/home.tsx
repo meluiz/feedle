9f01a5ea2b8e117bdbc545dd17aceb2cf30083ba
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2022-07-03 13:26:41 -0300

    homepage base

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import { FeedTextarea } from 'components'
+import { Box, Flex } from '@chakra-ui/react'
+
 const Page = function () {
-  return <div>Hello, World</div>
+  return (
+    <Flex flexDir="column" alignItems="flex-start" justifyContent="center" pos="relative">
+      <Box w="100%" borderBottom="1px solid" borderColor="mauve.1000" pos="relative">
+        <Box w="100%" h="100%" display="block" bg="inherit" pos="relative">
+          <FeedTextarea />
+        </Box>
+      </Box>
+    </Flex>
+  )
 }
 
 export default Page
